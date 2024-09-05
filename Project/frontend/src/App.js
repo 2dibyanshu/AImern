ff5f00b9253579766cca697128b8fc7e0a5d1202
Saranya <mslv.saranya@gmail.com>
2024-09-05 12:04:45 +0530

    Login page

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {/* Header */}
+        <header className="bg-blue-900 text-white p-4 flex justify-between items-center">
+          <div className="font-bold text-lg">
+            <h1>CustomCNN</h1>
+            <h2>by Squad of Syntax</h2>
+          </div>
+          <button className="bg-gray-200 text-blue-900 font-semibold px-4 py-1 rounded hover:bg-gray-300">
+            Register
+          </button>
+        </header>
+
+        {/* Form */}
+        <div className="px-8 py-6 mt-12 w-1/5 text-center">
+          <input
+            type="text"
+            placeholder="Username"
+            className="mb-4 w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+          /><br></br>
+          <input
+            type="password"
+            placeholder="Password"
+            className="mb-6 w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+          /><br></br>
+          <button className="bg-gray-200 text-blue-900 font-semibold w-40 py-2 rounded hover:bg-gray-300">
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
